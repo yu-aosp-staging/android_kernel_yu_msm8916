147745195b15515779d513980fe4ef79c27124d8
Scott Mertz <scott@cyngn.com>
2015-01-22 16:08:53 -0800

    power: smb1360: modify with wingtech changes
    * Squash of the following:
      - power: smb1360: t86519a1: implement charging temp thresholds
      - power: smb1360: set battery status to discharge when usb is unplugged.
      - power: smb1360: Fix battery capacity inaccuracy problem after resume.
      - power: smb1360: fixup handling of removal of power source
    
    Change-Id: I04c332ace5db3105b94554a02c764d5849d3325f

--- drivers/power/smb1360-charger-fg.c
+++ drivers/power/smb1360-charger-fg.c
@@ -413,6 +413,9 @@ struct smb1360_chip {
 	bool				batt_warm;
 	bool				batt_cool;
 	bool				batt_full;
+#ifdef CONFIG_MACH_T86519A1
+	bool				power_ok;
+#endif
 	bool				resume_completed;
 	bool				irq_waiting;
 	bool				irq_disabled;
@@ -509,6 +512,10 @@ static void smb1360_wakeup_src_init(struct smb1360_chip *chip)
 	spin_lock_init(&chip->smb1360_ws.ws_lock);
 	wakeup_source_init(&chip->smb1360_ws.source, "smb1360");
 }
+
+#ifdef CONFIG_MACH_T86519A1
+static int high_temp_chg = 1;
+#endif
 
 static int is_between(int value, int left, int right)
 {
@@ -1151,7 +1158,11 @@ static int smb1360_get_prop_batt_status(struct smb1360_chip *chip)
 	if (is_device_suspended(chip))
 		return POWER_SUPPLY_STATUS_UNKNOWN;
 
-	if (chip->batt_full)
+	if (chip->batt_full
+#ifdef CONFIG_MACH_T86519A1
+			&& chip->usb_present
+#endif
+	)
 		return POWER_SUPPLY_STATUS_FULL;
 
 	rc = smb1360_read(chip, STATUS_3_REG, &reg);
@@ -1161,6 +1172,11 @@ static int smb1360_get_prop_batt_status(struct smb1360_chip *chip)
 	}
 
 	pr_debug("STATUS_3_REG = %x\n", reg);
+
+#ifdef CONFIG_MACH_T86519A1
+	if (!chip->power_ok)
+		return POWER_SUPPLY_STATUS_DISCHARGING;
+#endif
 
 	if (reg & CHG_HOLD_OFF_BIT)
 		return POWER_SUPPLY_STATUS_NOT_CHARGING;
@@ -1248,6 +1264,13 @@ static int smb1360_get_prop_batt_capacity(struct smb1360_chip *chip)
 
 	pr_debug("msys_soc_reg=0x%02x, fg_soc=%d batt_full = %d\n", reg,
 						soc, chip->batt_full);
+
+#ifdef CONFIG_MACH_T86519A1
+	if (soc == 100 && chip->batt_full == 0)
+		chip->batt_full = 1;
+	else if (soc < 100 && chip->batt_full == 1)
+		chip->batt_full = 0;
+#endif
 
 	chip->soc_now = (chip->batt_full ? 100 : bound(soc, 0, 100));
 
@@ -2125,6 +2148,16 @@ static void smb1360_jeita_work_fn(struct work_struct *work)
 							jeita_work);
 	temp = smb1360_get_prop_batt_temp(chip);
 
+#ifdef CONFIG_MACH_T86519A1
+	if(!high_temp_chg && temp < chip->warm_bat_decidegc) {
+		pr_info("low temp threshold, enable charging\n");
+		smb1360_charging_disable(chip, USER, 0);
+		power_supply_changed(&chip->batt_psy);
+		power_supply_changed(chip->usb_psy);
+		high_temp_chg = 1;
+	}
+#endif
+
 	if (temp > chip->hot_bat_decidegc) {
 		/* battery status is hot, only config thresholds */
 		rc = smb1360_set_soft_jeita_threshold(chip,
@@ -2138,6 +2171,15 @@ static void smb1360_jeita_work_fn(struct work_struct *work)
 		/* battery status is warm, do compensation manually */
 		chip->batt_warm = true;
 		chip->batt_cool = false;
+#ifdef CONFIG_MACH_T86519A1
+		if(high_temp_chg && temp >= chip->hot_bat_decidegc) {
+			high_temp_chg = 0;
+			pr_info("high temp threshold, disable charging\n");
+			smb1360_charging_disable(chip, USER, 1);
+			power_supply_changed(&chip->batt_psy);
+			power_supply_changed(chip->usb_psy);
+		}
+#endif
 		rc = smb1360_float_voltage_set(chip, chip->warm_bat_mv);
 		if (rc) {
 			dev_err(chip->dev, "Couldn't set float voltage\n");
@@ -2577,6 +2619,15 @@ static int otg_oc_handler(struct smb1360_chip *chip, u8 rt_stat)
 	return 0;
 }
 
+#ifdef CONFIG_MACH_T86519A1
+static int power_ok_handler(struct smb1360_chip *chip, u8 rt_stat)
+{
+	pr_debug("xxx::usb in::rt_stat = 0x%02x\n", rt_stat);
+	chip->power_ok = rt_stat;
+	return 0;
+}
+#endif
+
 struct smb_irq_info {
 	const char		*name;
 	int			(*smb_irq)(struct smb1360_chip *chip,
@@ -2690,6 +2741,9 @@ static struct irq_handler_info handlers[] = {
 		{
 			{
 				.name		= "power_ok",
+#ifdef CONFIG_MACH_T86519A1
+				.smb_irq	= power_ok_handler,
+#endif
 			},
 			{
 				.name		= "unused",
