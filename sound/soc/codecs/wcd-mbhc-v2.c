b5a73cbe01e6c6073847632c2eb8e6c6b18a22ea
Ethan Chen <intervigil@gmail.com>
2015-10-13 21:32:08 -0700

    ASoC: msm: Add Yulong CP8675 support
    * Squash of the following:
    
    ASoC: msm: Remove failure for DMIC suspend/active lines
    
    ASoC: msm: Add I2S sample rate controls
    
    ASoC: msm: Support multiple headset buttons
    
    Fix calibration for multiple buttons.
    Remap headset buttons to keys.
    
    REF: MMX-53
    
    ASoC: wcd: Merge Yulong MBHC changes
    
    ASoC: msm: Adjust v_hs_max to detect more types of headsets
    
    ASoC: wcd: Increase system resume timeout
    
    * Certain devices require longer to resume from suspend
    REF: TOMATO-21
    
    ASoC: msm: cp8675: Shuffle pinctrl lookup to appropriate location
    
    misc: Update kernel from yulong
    
    ASoC: msm: Add tomato headset button thresholds
    
    Signed-off-by: Meninblack007 <sanyam.53jain@gmail.com>
    
    Change-Id: If0cdec0f32b23575f5f8e3d97030b512b8d88c3d

--- sound/soc/codecs/wcd-mbhc-v2.c
+++ sound/soc/codecs/wcd-mbhc-v2.c
@@ -2145,6 +2145,7 @@ int wcd_mbhc_init(struct wcd_mbhc *mbhc, struct snd_soc_codec *codec,
 	mbhc->is_extn_cable = false;
 	mbhc->hph_type = WCD_MBHC_HPH_NONE;
 	mbhc->wcd_mbhc_regs = wcd_mbhc_regs;
+	mbhc->jiffies_atreport = jiffies;
 
 	if (mbhc->intr_ids == NULL) {
 		pr_err("%s: Interrupt mapping not provided\n", __func__);
@@ -2185,6 +2186,18 @@ int wcd_mbhc_init(struct wcd_mbhc *mbhc, struct snd_soc_codec *codec,
 		ret = snd_jack_set_key(mbhc->button_jack.jack,
 				       SND_JACK_BTN_0,
 				       KEY_MEDIA);
+		ret = snd_jack_set_key(mbhc->button_jack.jack,
+				       SND_JACK_BTN_1,
+				       KEY_VOLUMEUP);
+		ret = snd_jack_set_key(mbhc->button_jack.jack,
+				       SND_JACK_BTN_2,
+				       KEY_VOLUMEDOWN);
+		ret = snd_jack_set_key(mbhc->button_jack.jack,
+				       SND_JACK_BTN_3,
+				       KEY_VOLUMEDOWN);
+		ret = snd_jack_set_key(mbhc->button_jack.jack,
+				       SND_JACK_BTN_4,
+				       KEY_VOLUMEDOWN);
 		if (ret) {
 			pr_err("%s: Failed to set code for btn-0\n",
 				__func__);
