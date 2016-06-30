706805a493f0a50829db6cf160670bda010849ec
Louis Popi <theh2o64@gmail.com>
2016-06-30 10:57:13 +0200

    input: touchscreen: gt9xx_yl: Mega cleanup and code refractor

--- drivers/input/touchscreen/gt9xx_yl/gt9xx_config.h
+++ drivers/input/touchscreen/gt9xx_yl/gt9xx_config.h
@@ -1,45 +1,41 @@
+/* 
+ * 2010 - 2012 Goodix Technology.
+ * 2016 - Louis Popi <theh2o64@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be a reference
+ * to you, when you are integrating the GOODiX's CTP IC into your system,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ */
+
 #ifndef _LINUX_GOODIX_CONFIG_H
 #define	_LINUX_GOODIX_CONFIG_H
 
+// Define panel model
 #ifdef CONFIG_MACH_TOMATO
 const char * TW_IC_PREFIX_NAME = "GT970";
 #else
 const char * TW_IC_PREFIX_NAME = "GT9158";
 #endif
-//***************************PART1:ON/OFF define*******************************
-#define GTP_CUSTOM_CFG        1
-#define GTP_DRIVER_SEND_CFG   1
-#define GTP_HAVE_TOUCH_KEY    1
-#define GTP_POWER_CTRL_SLEEP  0
-#define GTP_AUTO_UPDATE       1
-#define GTP_CHANGE_X2Y        0
 
+// Firmware auto update
+#define GTP_AUTO_UPDATE 1
 
-/* begin to delete this macro and it will be define in include/linux/gt9xx.h by liushilong@yulong.com on 2014-11-6 14:57*/
-//#define GTP_ESD_PROTECT       1
-/* end to delete this macro and it will be define in include/linux/gt9xx.h by liushilong@yulong.com on 2014-11-6 14:57*/
+// Multitouch limited to 
+#define GTP_HAVE_TOUCH_KEY 1
+#define GTP_MAX_TOUCH 5
 
-
-#define GTP_CREATE_WR_NODE    1
-#define GTP_ICS_SLOT_REPORT   0
-
-#define GUP_USE_HEADER_FILE   0
-
-#define GTP_SLIDE_WAKEUP      1
-
-#define GTP_DBL_CLK_WAKEUP    0
-#define YL_COVER_SWITCH_FUNC  0
-
-
-
-#define GTP_MAX_TOUCH         5
-
-/* begin to transplant TW GTP_ESD_PROTECT code from 8675_C00, 1s GTP_ESD_CHECK_CIRCLE * HZ, it will be transform to jiffy by liushilong@yulong.com on 2014-11-6 15:00*/
+// ESD
 #define GTP_ESD_CHECK_CIRCLE  2
-/* begin to transplant TW GTP_ESD_PROTECT code from 8675_C00, 1s GTP_ESD_CHECK_CIRCLE * HZ, it will be transform to jiffy by liushilong@yulong.com on 2014-11-6 15:00*/
 
-//#define TW_GLOVE_SWITCH       0
-
+// Firmware
 #ifdef CONFIG_MACH_TOMATO
 #define GTP_COB		      1
 #else
@@ -57,15 +53,12 @@ struct touch_panel_info
 	unsigned int   firmware_size; 
 };
 
-//[YULONG BEGIN] Upload new touchscreen configs to solve touchscreen failure in games occationally by litao3@yulong.com 2014-12-23
+
+// Touchscreen firmwares
 #ifdef CONFIG_MACH_TOMATO
 static unsigned char CTP_CFG_GROUP1[]=  
 {
     #include "coolpad_8675_00_V46_20141111_finger.cfg"
-};
-static unsigned char CTP_CFG_GROUP1_glove[]=  
-{
-    #include "coolpad_8675_00_V46_20141111_glove.cfg"
 };
 #ifdef CONFIG_TOUCHSCREEN_GT9XX_YL_COVER_WINDOW_CFG
 static unsigned char CTP_CFG_GROUP1_window[]=  
@@ -73,14 +66,9 @@ static unsigned char CTP_CFG_GROUP1_window[]=
     #include "coolpad_8675_00_V46_20141111_window.cfg"
 };
 #endif
-
 static unsigned char CTP_CFG_GROUP2[]=  
 {
     #include "coolpad_8675_02_V46_20141111_finger.cfg"
-};
-static unsigned char CTP_CFG_GROUP2_glove[]=  
-{
-    #include "coolpad_8675_02_V46_20141111_glove.cfg"
 };
 #ifdef CONFIG_TOUCHSCREEN_GT9XX_YL_COVER_WINDOW_CFG
 static unsigned char CTP_CFG_GROUP2_window[]=  
@@ -88,14 +76,9 @@ static unsigned char CTP_CFG_GROUP2_window[]=
     #include "coolpad_8675_02_V46_20141111_window.cfg"
 };
 #endif
-
 static unsigned char CTP_CFG_GROUP3[]=  
 {
     #include "coolpad_8675_01_V46_20141111_finger.cfg"
-};
-static unsigned char CTP_CFG_GROUP3_glove[]=  
-{
-    #include "coolpad_8675_01_V46_20141111_glove.cfg"
 };
 #ifdef CONFIG_TOUCHSCREEN_GT9XX_YL_COVER_WINDOW_CFG
 static unsigned char CTP_CFG_GROUP3_window[]=  
@@ -111,11 +94,6 @@ struct touch_panel_info yl_cfg[] = {
   {0x01, "Each", CTP_CFG_GROUP3, sizeof(CTP_CFG_GROUP3)},
 };
 
-struct touch_panel_info yl_cfg_glove[] = {
-  {0x00, "Boen", CTP_CFG_GROUP1_glove, sizeof(CTP_CFG_GROUP1_glove)},
-  {0x02, "Ofilm", CTP_CFG_GROUP2_glove, sizeof(CTP_CFG_GROUP2_glove)},
-  {0x01, "Each", CTP_CFG_GROUP3_glove, sizeof(CTP_CFG_GROUP3_glove)},
-};
 #ifdef CONFIG_TOUCHSCREEN_GT9XX_YL_COVER_WINDOW_CFG
 struct touch_panel_info yl_cfg_window[] = {
   {0x00, "Boen", CTP_CFG_GROUP1_window, sizeof(CTP_CFG_GROUP1_window)},
@@ -129,39 +107,18 @@ static unsigned char CTP_CFG_GROUP1[]=
 {
 	#include "coolapd_7625_00_V47_20140730_finger.cfg"
 };
-static unsigned char CTP_CFG_GROUP1_glove[]=  
-{
-	#include "coolapd_7625_00_V47_20140730_glove.cfg"
-};
-
-
- static unsigned char CTP_CFG_GROUP2[]=
+static unsigned char CTP_CFG_GROUP2[]=
 {
    #include "coolapd_7625_01_V47_20140730_finger.cfg"
 };
- static unsigned char CTP_CFG_GROUP2_glove[]=
-{
-   #include "coolapd_7625_01_V47_20140730_glove.cfg"
-};
-
 static unsigned char CTP_CFG_GROUP3[]=
 {
     #include "coolapd_7625_02_V47_20140730_finger.cfg"
 };
-static unsigned char CTP_CFG_GROUP3_glove[]=
-{
-    #include "coolapd_7625_02_V47_20140730_glove.cfg"  
-};
-
 struct touch_panel_info yl_cfg[] = {
   {0x00, "boen", CTP_CFG_GROUP1, sizeof(CTP_CFG_GROUP1)},
   {0x01, "junda", CTP_CFG_GROUP2, sizeof(CTP_CFG_GROUP2)},
   {0x02, "ofilm", CTP_CFG_GROUP3, sizeof(CTP_CFG_GROUP3)},
-};
-struct touch_panel_info yl_cfg_glove[] = {
-  {0x00, "boen", CTP_CFG_GROUP1_glove, sizeof(CTP_CFG_GROUP1_glove)},
-  {0x01, "junda", CTP_CFG_GROUP2_glove, sizeof(CTP_CFG_GROUP2_glove)},
-  {0x02, "ofilm", CTP_CFG_GROUP3_glove, sizeof(CTP_CFG_GROUP3_glove)},  
 };
 #endif
 
