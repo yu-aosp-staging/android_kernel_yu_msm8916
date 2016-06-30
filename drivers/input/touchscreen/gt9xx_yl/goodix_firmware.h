42d9b038cc1e5dea0609a407fa03b69a93daafd6
Louis Popi <theh2o64@gmail.com>
2016-06-30 10:57:13 +0200

    input: touchscreen: gt9xx_yl: Mega cleanup and code refractor

--- drivers/input/touchscreen/gt9xx_yl/goodix_firmware.h
+++ drivers/input/touchscreen/gt9xx_yl/goodix_firmware.h
@@ -1,3 +1,20 @@
+/* 
+ * 2010 - 2012 Goodix Technology.
+ * 2016 - Louis Popi <theh2o64@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be a reference
+ * to you, when you are integrating the GOODiX's CTP IC into your system,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ */
+
 #ifndef LINUX_GT9XX_FIRMWARE_H
 #define LINUX_GT9XX_FIRMWARE_H
 
