47e15eb9468f49fa4e7df4757d33fb94d8ca6c75
Louis Popi <theh2o64@gmail.com>
2016-11-12 07:39:39 +0100

    power: yl_bq24157: Report technology and current now

---
+++ pick.sh
@@ -0,0 +1,8 @@
+git cherry-pick 5c0dda3274cdd032832d670e372e5e627caa3e3b # KEYS: Fix short sprintf buffer in /proc/keys show function
+git cherry-pick 5d81ce7c8daef88af75d392f0cbc1552413e7728 # defconfigs: Update YU defconfigs
+git cherry-pick 55d04518f66441cb87f14511aff7dfa0d1fef2e2 # defconfig: Restore SND_SOC_WSA881X on MSM8916
+git cherry-pick cc1aeb0cab5ef41a82689b15aff5d67e8e81c54d # sound: wsa881x-regmap: Use new reg_sequence struct
+git cherry-pick 18932bdbf00d919425da6f71d4ed18accbbbaaf4 # include: regmap: Add regmap init functions to includes
+git cherry-pick 5d8ed9ba94f895d53bb9560847d51b644b2e7784 # splash: Update to 500x500
+git cherry-pick 19722c887476f543b8d02731f67a884d8e4cdda8 # Revert "dtb: Update panels from 3.10.49.00.P0.160314.LMY47V"
+git cherry-pick 1f5d213fb23586217535750352cfc305fb0dc14a # configs: Add TCP Congestion algorithm
